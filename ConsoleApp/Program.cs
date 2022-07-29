ab27632a42b662c05f300056288ba48dc4a64fa9
Dmitry Abramov <abramov.dmittry@gmail.com>
2022-07-29 13:28:22 +0300

    Реализация IssueEnumerator

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -18,10 +19,106 @@ namespace ConsoleApp
         public List<Issue> Issues { get; set; }
     }
 
+    public class Issues : IEnumerable<Issue>
+    {
+        private Issue[] _issues;
+
+        public Issues(Issue[] issues)
+        {
+            _issues = issues;
+        }
+
+        public IEnumerator<Issue> GetEnumerator()
+        {
+            return new IssueEnumerator(_issues);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return new IssueEnumerator(_issues);
+        }
+    }
+
+    public class IssueEnumerator : IEnumerator<Issue>, IEnumerator
+    {
+        private Issue[] _issues;
+        private int _index;
+
+        public IssueEnumerator(Issue[] issues)
+        {
+            _issues = issues;
+            _index = -1;
+        }
+
+        public Issue Current => _issues[_index];
+
+        object IEnumerator.Current => _issues[_index];
+
+        public bool MoveNext()
+        {
+            _index++;
+
+            return _index < _issues.Length;
+        }
+
+        public void Reset()
+        {
+            _index = 0;
+        }
+
+        public void Dispose()
+        {
+        }
+    }
 
     class Program
     {
         static void Main(string[] args)
+        {
+            var issues = new[]
+            {
+                new Issue
+                {
+                    Name = "Test A",
+                    Description = "Test description",
+                    Deadline = DateTime.Now.AddDays(-2)
+                },
+                new Issue
+                {
+                    Name = "Test B",
+                    Description = "Test description",
+                    Deadline = DateTime.Now.AddDays(-1)
+                },
+                new Issue
+                {
+                    Name = "Test C",
+                    Description = "Test description",
+                    Deadline = DateTime.Now
+                },
+                new Issue
+                {
+                    Name = "Test D",
+                    Description = "Test description",
+                    Deadline = DateTime.Now.AddDays(1)
+                }
+            };
+
+            var data = new Issues(issues);
+
+            var query = data.Where(x => x.Deadline > DateTime.Now);
+
+            var result = query.ToArray();
+            var count = query .Count();
+
+            foreach (var item in result)
+            {
+                Console.WriteLine(item.Name + " " + item.Deadline);
+            }
+            Console.WriteLine();
+            Console.WriteLine(count);
+        }
+
+        private static void DataQueryInvocation()
         {
             var data = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 
@@ -39,7 +136,6 @@ namespace ConsoleApp
             Console.WriteLine();
 
             Console.WriteLine(result2);
-
         }
 
         public static void AnonimusTypes()
