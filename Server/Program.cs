5e9dfcf62703559eb8995ae0012fe5e80793e037
Dmitry Abramov <abramov.dmittry@gmail.com>
2022-08-01 20:12:20 +0300

    Реализуем клиент серверное взаимодействие с нуля на C#. Домашнее задание.

--- Server/Program.cs
+++ Server/Program.cs
@@ -5,15 +5,41 @@ namespace Server
 {
     internal class Program
     {
+        /// <summary>
+        /// https://github.com/emedvedeva/CsharpForBeginners/tree/main/Shop
+        /// https://github.com/emedvedeva/CsharpForBeginners/blob/main/HomeTask5/Program.cs
+        /// https://github.com/coder1coder/Shop
+        /// https://github.com/trulander/CsharpEducation/tree/main/ShowCase
+        /// https://github.com/Z1pP/CsharpForBeginners/tree/master/Shop
+        /// 
+        /// Fork одного из проектов.
+        /// 
+        /// Добавить магазин, добавить работу через клиент сервер.   
+        /// </summary>
+        /// <param name="args"></param>
+
+
         static void Main(string[] args)
         {
-            var httpClient = new HttpClient();
-            httpClient.DefaultRequestHeaders.Add("TEST_HEADER", "TEST VALUE");
+            var httpListener = new HttpListener();
+            httpListener.Prefixes.Add("http://localhost:51369/");
+            httpListener.Start();
 
-            var response = httpClient.PostAsync("http://localhost:51369", null).Result;
-            var content = response.Content.ReadAsStringAsync().Result;
+            while (true)
+            {
+                var requestContext = httpListener.GetContext();
+                requestContext.Response.StatusCode = 200;
 
-            Console.WriteLine(content);
+                var stream = requestContext.Response.OutputStream;
+
+                var text = "test messege";
+                var bytes = Encoding.UTF8.GetBytes(text);
+                stream.Write(bytes, 0, bytes.Length);
+                requestContext.Response.Close();
+            }
+
+            httpListener.Stop();
+            httpListener.Close();
         }
     }
 }
